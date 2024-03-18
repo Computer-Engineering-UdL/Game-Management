3dfb82a3d351957645d82f0d5029ee6d1979dbdf
Aniol0012 <aniol0012@gmail.com>
2024-03-18 13:58:39 +0100

    refactor: ruff lint

--- KeepGaming/settings.py
+++ KeepGaming/settings.py
@@ -122,6 +122,5 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
-
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
