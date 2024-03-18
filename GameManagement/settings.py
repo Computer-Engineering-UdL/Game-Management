75452d9888b78cd81f4db856fbdf382ec7f1e183
Aniol0012 <aniol0012@gmail.com>
2024-03-18 15:00:13 +0100

    feat: add login-logout auth

--- GameManagement/settings.py
+++ GameManagement/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "GameManagement.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
