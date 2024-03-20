5b5aaf2760108b05dbe5351e20f4614523b6492f
Aniol0012 <aniol0012@gmail.com>
2024-03-20 23:47:33 +0100

    fix: add library to installed apps and url

--- GameManagement/urls.py
+++ GameManagement/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("library/", include("library.urls")),
 ]
