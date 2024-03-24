99c6d3d55d6e05a1c0bd4ea61b5d07532f0a81b8
Aniol0012 <aniol0012@gmail.com>
2024-03-24 21:48:03 +0100

    feat: add library

---
+++ GameLibrary/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from GameLibrary.models import Game, Post
+
+# Register your models here.
+
+admin.site.register(Game)
+admin.site.register(Post)
---
+++ GameLibrary/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Game
+
+
+class GameListView(ListView):
+    model = Game
+    template_name = "games/game_list.html"
+    context_object_name = "games"
